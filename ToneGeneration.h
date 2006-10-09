d482404695d530883eee69eeab60468aca89b9ba
Dean Camera <dean@fourwalledcubicle.com>
2006-10-09 06:58:57 +0000

    Changed ringbuffer access to atomic.

---
+++ ToneGeneration.h
@@ -0,0 +1,47 @@
+/*
+             BUTTLOAD - Butterfly ISP Programmer
+				
+              Copyright (C) Dean Camera, 2006.
+                  dean_camera@hotmail.com
+*/
+
+#ifndef TONEGENERATION_H
+#define TONEGENERATION_H
+
+	// INCLUDES:
+	#include <avr/io.h>
+	#include <avr/pgmspace.h>
+	
+	#include "EEPROMVariables.h"
+	#include "GlobalMacros.h"
+	#include "Main.h"
+	
+	// DEFINES AND MACROS:
+	#define TONEGEN_SEQ_STARTUP    &ToneSeq_Startup[0]
+	#define TONEGEN_SEQ_SYNCFAIL   &ToneSeq_SyncFail[0]
+	#define TONEGEN_SEQ_PROGDONE   &ToneSeq_ProgDone[0]
+	#define TONEGEN_SEQ_PROGFAIL   &ToneSeq_ProgFail[0]
+	#define TONEGEN_SEQ_VOLTEST    &ToneSeq_VolTest[0]
+	#define TONEGEN_SEQ_SLEEP      &ToneSeq_Sleep[0]
+	#define TONEGEN_SEQ_RESUME     &ToneSeq_Resume[0]
+	#define TONEGEN_SEQ_ERROR      &ToneSeq_Error[0]
+	
+	#define TONEGEN_GET_TONE_VOL() MACROS{ ToneVol = eeprom_read_byte(&EEPROMVars.ToneVolume); }MACROE
+	
+	// EXTERNAL VARIABLES:
+	extern uint8_t  ToneSeq_Startup[]  PROGMEM;
+	extern uint8_t  ToneSeq_SyncDone[] PROGMEM;
+	extern uint8_t  ToneSeq_SyncFail[] PROGMEM;
+	extern uint8_t  ToneSeq_ProgDone[] PROGMEM;
+	extern uint8_t  ToneSeq_ProgFail[] PROGMEM;
+	extern uint8_t  ToneSeq_VolTest[]  PROGMEM;
+	extern uint8_t  ToneSeq_Sleep[]    PROGMEM;
+	extern uint8_t  ToneSeq_Resume[]   PROGMEM;	
+	extern uint8_t  ToneSeq_Error[]    PROGMEM;	
+	
+	extern uint8_t  ToneVol;
+	
+	// PROTOTYPES:
+	void TG_PlayToneSeq(uint8_t* Sequence);
+
+#endif
